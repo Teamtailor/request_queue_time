4413f642df89fbab35d7df774a5fc7797b358037
Björn Nordstrand <bjorn@def-end.se>
2024-09-02 11:52:00 +0200

    Teamtailor had issues with pointing to the correct careersite
    Because the section io host middleware ended up too far down
    the stack after the change.
    
    So we have had to move up the request queue middleware to the top of the stack.
    
    Co-authored-by: Jonas B <jonas@teamtailor.com>

--- lib/request_queue_time/railtie.rb
+++ lib/request_queue_time/railtie.rb
@@ -1,7 +1,7 @@
 module RequestQueueTime
   class Railtie < Rails::Railtie
     initializer "RequestQueueTime.request_middleware" do |app|
-      app.middleware.insert_before Rack::Runtime, RequestQueueTime::Middleware
+      app.middleware.insert_after 0, RequestQueueTime::Middleware
     end
   end
 end
